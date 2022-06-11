e4b09ebe1eeac5f083686554a7c30cdea1d8e1e8
Bhfeirdi <ferdia.link@gmail.com>
2022-06-11 22:29:04 +0100

    var check dead collision added

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 var hero = document.getElementById("hero");
-
 var block = document.getElementById("block");
-
+var counter=0;
 function jump(){
+    if(hero.classList == "animate"){return}
     hero.classList.add("animate");
-    setTimeout(function()){
-        hero.classList.remove("animate");
-    } ,500);
-    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
+var checkDead = setInterval(function() {
+    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && heroTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
