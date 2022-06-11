17e856d0a4ee7dd38221777ace6f764126a765f9
Bhfeirdi <ferdia.link@gmail.com>
2022-06-11 19:40:59 +0100

    timeout aded to jump funtion

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ var block = document.getElementById("block");
 
 function jump(){
     hero.classList.add("animate");
+    setTimeout(function()){
+        CharacterData.classList.remove("animate");
+    } ,500);
 }
